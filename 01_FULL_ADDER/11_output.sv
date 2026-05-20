506782369210cbf06912cc3448ec28b74659ae54
Sakthivel S <srsakthivel531@gmail.com>
2026-05-20 23:54:35 +0530

    Create 11_output.sv

---
+++ 01_FULL_ADDER/11_output.sv
@@ -0,0 +1,75 @@
+# KERNEL: _________GENERATOR SIGNAL__________
+# KERNEL: a=0 b=0 cin=0 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: _________DRIVER  SIGNALS__________
+# KERNEL: a=0 b=0 cin=0 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: _________MONITOR  SIGNALS__________
+# KERNEL: a=0 b=0 cin=0 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: _________SCOREBOARD SIGNALS__________
+# KERNEL: a=0 b=0 cin=0 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: =====================================
+# KERNEL: VERIFICATION PASSED
+# KERNEL: =====================================
+# KERNEL: _________GENERATOR SIGNAL__________
+# KERNEL: a=1 b=1 cin=0 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: _________DRIVER  SIGNALS__________
+# KERNEL: a=1 b=1 cin=0 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: _________MONITOR  SIGNALS__________
+# KERNEL: a=1 b=1 cin=0 sum=0 carry=1
+# KERNEL: =====================================
+# KERNEL: _________SCOREBOARD SIGNALS__________
+# KERNEL: a=1 b=1 cin=0 sum=0 carry=1
+# KERNEL: =====================================
+# KERNEL: =====================================
+# KERNEL: VERIFICATION PASSED
+# KERNEL: =====================================
+# KERNEL: _________GENERATOR SIGNAL__________
+# KERNEL: a=1 b=1 cin=1 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: _________DRIVER  SIGNALS__________
+# KERNEL: a=1 b=1 cin=1 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: _________MONITOR  SIGNALS__________
+# KERNEL: a=1 b=1 cin=1 sum=1 carry=1
+# KERNEL: =====================================
+# KERNEL: _________SCOREBOARD SIGNALS__________
+# KERNEL: a=1 b=1 cin=1 sum=1 carry=1
+# KERNEL: =====================================
+# KERNEL: =====================================
+# KERNEL: VERIFICATION PASSED
+# KERNEL: =====================================
+# KERNEL: _________GENERATOR SIGNAL__________
+# KERNEL: a=0 b=0 cin=1 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: _________DRIVER  SIGNALS__________
+# KERNEL: a=0 b=0 cin=1 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: _________MONITOR  SIGNALS__________
+# KERNEL: a=0 b=0 cin=1 sum=1 carry=0
+# KERNEL: =====================================
+# KERNEL: _________SCOREBOARD SIGNALS__________
+# KERNEL: a=0 b=0 cin=1 sum=1 carry=0
+# KERNEL: =====================================
+# KERNEL: =====================================
+# KERNEL: VERIFICATION PASSED
+# KERNEL: =====================================
+# KERNEL: _________GENERATOR SIGNAL__________
+# KERNEL: a=1 b=1 cin=1 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: _________DRIVER  SIGNALS__________
+# KERNEL: a=1 b=1 cin=1 sum=0 carry=0
+# KERNEL: =====================================
+# KERNEL: _________MONITOR  SIGNALS__________
+# KERNEL: a=1 b=1 cin=1 sum=1 carry=1
+# KERNEL: =====================================
+# KERNEL: _________SCOREBOARD SIGNALS__________
+# KERNEL: a=1 b=1 cin=1 sum=1 carry=1
+# KERNEL: =====================================
+# KERNEL: =====================================
+# KERNEL: VERIFICATION PASSED
+# KERNEL: =====================================
