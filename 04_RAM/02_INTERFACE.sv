25569cf53a51e19a856fb90f2d91e10eac8bc3f8
Sakthivel S <srsakthivel531@gmail.com>
2026-05-29 15:44:41 +0530

    Update 02_INTERFACE.sv

--- 04_RAM/02_INTERFACE.sv
+++ 04_RAM/02_INTERFACE.sv
@@ -5,13 +5,23 @@ logic w_en;
 logic[ADDR_WIDTH-1:0] address;
 logic[DATA_WIDTH-1:0] w_data;
 logic[DATA_WIDTH-1:0] r_data;
-clocking cb @(posedge clk);
+  
+clocking dri_cb @(posedge clk);
 default input #1step output #0;
 output rst;
 output r_en;
 output w_en;
 output address;
 output w_data;
+endclocking 
+  
+clocking mon_cb @(posedge clk);
+default input #1step;
+input  rst;
+input r_en;
+input w_en;
+input address;
+input w_data;
 input r_data;
 endclocking 
 endinterface
