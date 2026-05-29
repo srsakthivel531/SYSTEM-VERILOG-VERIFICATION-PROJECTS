0ba9a617947fa1e5f90681553235e1bf05e06539
Sakthivel S <srsakthivel531@gmail.com>
2026-05-29 15:45:10 +0530

    Update 04_GENERATOR.sv

--- 04_RAM/04_GENERATOR.sv
+++ 04_RAM/04_GENERATOR.sv
@@ -1,22 +1,17 @@
 class generator;
 transaction trans;
 mailbox #(transaction)gen_to_dri;
-virtual s_ram sr;
 
-function new(mailbox #(transaction) gen_to_dri,virtual s_ram sr);
+
+function new(mailbox #(transaction) gen_to_dri);
 this.gen_to_dri=gen_to_dri;
-this.sr=sr;
 endfunction
   
 task run();
 repeat(20) begin
-@(sr.cb);
 trans=new();
 trans.randomize();
 gen_to_dri.put(trans);
-$display("__________________________________________________________________________________________________________");
-$display("===================GENERATOR SIGNALS==================");
-$display("TIME=%0t W_EN=%0d r_EN=%0d ADDRESS=%0d w_DATA=%0d",$time,trans.w_en,trans.r_en,trans.address,trans.w_data);
 end 
 endtask
 endclass 
