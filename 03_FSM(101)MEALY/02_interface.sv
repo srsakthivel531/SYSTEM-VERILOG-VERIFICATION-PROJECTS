c7e4926c3517c11a55d21fb89bfa511d134f6aac
Sakthivel S <srsakthivel531@gmail.com>
2026-05-29 15:43:49 +0530

    Update 02_interface.sv

--- 03_FSM(101)MEALY/02_interface.sv
+++ 03_FSM(101)MEALY/02_interface.sv
@@ -1,16 +1,27 @@
-interface seq_inf(input logic clk);
-logic x;
-logic reset;
-logic y;
-// Clocking block
-clocking drv_cb @(posedge clk);
+interface s_ram#(parameter ADDR_WIDTH=4,DATA_WIDTH=8)(input logic clk);
+logic rst;
+logic r_en;
+logic w_en;
+logic[ADDR_WIDTH-1:0] address;
+logic[DATA_WIDTH-1:0] w_data;
+logic[DATA_WIDTH-1:0] r_data;
+  
+clocking dri_cb @(posedge clk);
 default input #1step output #0;
-output x,reset;
-input y;
-endclocking
+output rst;
+output r_en;
+output w_en;
+output address;
+output w_data;
+endclocking 
+  
 clocking mon_cb @(posedge clk);
 default input #1step;
-input x,reset;
-input y;
-endclocking
-endinterface 
+input  rst;
+input r_en;
+input w_en;
+input address;
+input w_data;
+input r_data;
+endclocking 
+endinterface
