7cffb846c7cbb8b5a67edf924b96923d6a4bf2c4
Sakthivel S <srsakthivel531@gmail.com>
2026-05-29 13:28:24 +0530

    Create 10_TOP_MODULE.sv

---
+++ 04_RAM/10_TOP_MODULE.sv
@@ -0,0 +1,18 @@
+`include "interface.sv"
+`include "program_block.sv"
+module tb;
+logic clk;
+s_ram sr(clk);
+sram dut(sr);
+test t(sr);
+initial begin 
+clk=0;
+forever #1 clk=~clk;
+end 
+initial begin 
+sr.rst=1;
+@(posedge clk);
+sr.rst=0;
+#1000 $finish;
+end 
+endmodule 
