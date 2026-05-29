8b34a06bd52038e3cf6845afd9beebf561818c96
Sakthivel S <srsakthivel531@gmail.com>
2026-05-29 15:47:34 +0530

    Update 10_TOP_MODULE.sv

--- 04_RAM/10_TOP_MODULE.sv
+++ 04_RAM/10_TOP_MODULE.sv
@@ -2,13 +2,16 @@
 `include "program_block.sv"
 module tb;
 logic clk;
+  
 s_ram sr(clk);
 sram dut(sr);
 test t(sr);
+ 
 initial begin 
 clk=0;
 forever #1 clk=~clk;
 end 
+ 
 initial begin 
 sr.rst=1;
 @(posedge clk);
