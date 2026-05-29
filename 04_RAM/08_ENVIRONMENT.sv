7511e5de2afd78ad433bb0f571b97a016127426a
Sakthivel S <srsakthivel531@gmail.com>
2026-05-29 13:27:09 +0530

    Create 08_ENVIRONMENT.sv

---
+++ 04_RAM/08_ENVIRONMENT.sv
@@ -0,0 +1,34 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+
+class environment;
+transaction trans;
+generator gen;
+monitor mon;
+driver dri;
+scoreboard sco;
+mailbox #(transaction) mon_to_sco;
+mailbox #(transaction)gen_to_dri;
+virtual s_ram sr;
+function new(virtual s_ram sr);
+this.sr=sr;
+mon_to_sco=new();
+gen_to_dri=new();
+gen=new(gen_to_dri,sr);
+dri=new(gen_to_dri,sr);
+mon=new(mon_to_sco,sr);
+sco=new(mon_to_sco);
+endfunction 
+task t();
+fork 
+gen.run();
+dri.run();
+mon.run();
+sco.run();
+join
+endtask
+endclass
+  
