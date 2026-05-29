7e27b8739293d914d926c3a7c1c42ea52ed2078a
Sakthivel S <srsakthivel531@gmail.com>
2026-05-29 13:29:09 +0530

    Create 11_OUTPUT.sv

---
+++ 04_RAM/11_OUTPUT.sv
@@ -0,0 +1,219 @@
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=1 W_EN=1 r_EN=0 ADDRESS=12 w_DATA=44
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=1 W_EN=1 r_EN=0 ADDRESS=12 w_DATA=44
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=1 W_EN=0 r_EN=0 ADDRESS=0 w_DATA=0 R_DATA=0
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=1 W_EN=0 r_EN=0 ADDRESS=0 w_DATA=0,R_DATA=0
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=3 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=246
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=3 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=246
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=3 W_EN=1 r_EN=0 ADDRESS=12 w_DATA=44 R_DATA=0
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=3 W_EN=1 r_EN=0 ADDRESS=12 w_DATA=44,R_DATA=0
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=5 W_EN=1 r_EN=0 ADDRESS=3 w_DATA=148
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=5 W_EN=1 r_EN=0 ADDRESS=3 w_DATA=148
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=5 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=246 R_DATA=0
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=5 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=246,R_DATA=0
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=7 W_EN=1 r_EN=0 ADDRESS=13 w_DATA=210
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=7 W_EN=1 r_EN=0 ADDRESS=13 w_DATA=210
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=7 W_EN=1 r_EN=0 ADDRESS=3 w_DATA=148 R_DATA=0
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=7 W_EN=1 r_EN=0 ADDRESS=3 w_DATA=148,R_DATA=0
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=9 W_EN=1 r_EN=0 ADDRESS=12 w_DATA=220
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=9 W_EN=1 r_EN=0 ADDRESS=12 w_DATA=220
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=9 W_EN=1 r_EN=0 ADDRESS=13 w_DATA=210 R_DATA=0
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=9 W_EN=1 r_EN=0 ADDRESS=13 w_DATA=210,R_DATA=0
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=11 W_EN=0 r_EN=1 ADDRESS=3 w_DATA=91
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=11 W_EN=0 r_EN=1 ADDRESS=3 w_DATA=91
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=11 W_EN=1 r_EN=0 ADDRESS=12 w_DATA=220 R_DATA=0
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=11 W_EN=1 r_EN=0 ADDRESS=12 w_DATA=220,R_DATA=0
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=13 W_EN=1 r_EN=0 ADDRESS=14 w_DATA=220
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=13 W_EN=1 r_EN=0 ADDRESS=14 w_DATA=220
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=13 W_EN=0 r_EN=1 ADDRESS=3 w_DATA=91 R_DATA=148
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=13 W_EN=0 r_EN=1 ADDRESS=3 w_DATA=91,R_DATA=148
+# KERNEL: READ OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=15 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=193
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=15 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=193
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=15 W_EN=1 r_EN=0 ADDRESS=14 w_DATA=220 R_DATA=148
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=15 W_EN=1 r_EN=0 ADDRESS=14 w_DATA=220,R_DATA=148
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=17 W_EN=1 r_EN=0 ADDRESS=5 w_DATA=38
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=17 W_EN=1 r_EN=0 ADDRESS=5 w_DATA=38
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=17 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=193 R_DATA=148
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=17 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=193,R_DATA=148
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=19 W_EN=1 r_EN=0 ADDRESS=2 w_DATA=162
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=19 W_EN=1 r_EN=0 ADDRESS=2 w_DATA=162
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=19 W_EN=1 r_EN=0 ADDRESS=5 w_DATA=38 R_DATA=148
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=19 W_EN=1 r_EN=0 ADDRESS=5 w_DATA=38,R_DATA=148
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=21 W_EN=1 r_EN=0 ADDRESS=14 w_DATA=95
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=21 W_EN=1 r_EN=0 ADDRESS=14 w_DATA=95
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=21 W_EN=1 r_EN=0 ADDRESS=2 w_DATA=162 R_DATA=148
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=21 W_EN=1 r_EN=0 ADDRESS=2 w_DATA=162,R_DATA=148
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=23 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=180
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=23 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=180
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=23 W_EN=1 r_EN=0 ADDRESS=14 w_DATA=95 R_DATA=148
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=23 W_EN=1 r_EN=0 ADDRESS=14 w_DATA=95,R_DATA=148
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=25 W_EN=1 r_EN=0 ADDRESS=5 w_DATA=138
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=25 W_EN=1 r_EN=0 ADDRESS=5 w_DATA=138
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=25 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=180 R_DATA=148
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=25 W_EN=1 r_EN=0 ADDRESS=7 w_DATA=180,R_DATA=148
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=27 W_EN=0 r_EN=1 ADDRESS=3 w_DATA=163
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=27 W_EN=0 r_EN=1 ADDRESS=3 w_DATA=163
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=27 W_EN=1 r_EN=0 ADDRESS=5 w_DATA=138 R_DATA=148
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=27 W_EN=1 r_EN=0 ADDRESS=5 w_DATA=138,R_DATA=148
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=29 W_EN=0 r_EN=1 ADDRESS=12 w_DATA=165
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=29 W_EN=0 r_EN=1 ADDRESS=12 w_DATA=165
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=29 W_EN=0 r_EN=1 ADDRESS=3 w_DATA=163 R_DATA=148
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=29 W_EN=0 r_EN=1 ADDRESS=3 w_DATA=163,R_DATA=148
+# KERNEL: READ OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=31 W_EN=0 r_EN=1 ADDRESS=4 w_DATA=83
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=31 W_EN=0 r_EN=1 ADDRESS=4 w_DATA=83
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=31 W_EN=0 r_EN=1 ADDRESS=12 w_DATA=165 R_DATA=220
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=31 W_EN=0 r_EN=1 ADDRESS=12 w_DATA=165,R_DATA=220
+# KERNEL: READ OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=33 W_EN=0 r_EN=1 ADDRESS=2 w_DATA=93
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=33 W_EN=0 r_EN=1 ADDRESS=2 w_DATA=93
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=33 W_EN=0 r_EN=1 ADDRESS=4 w_DATA=83 R_DATA=0
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=33 W_EN=0 r_EN=1 ADDRESS=4 w_DATA=83,R_DATA=0
+# KERNEL: READ OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=35 W_EN=0 r_EN=1 ADDRESS=5 w_DATA=246
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=35 W_EN=0 r_EN=1 ADDRESS=5 w_DATA=246
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=35 W_EN=0 r_EN=1 ADDRESS=2 w_DATA=93 R_DATA=162
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=35 W_EN=0 r_EN=1 ADDRESS=2 w_DATA=93,R_DATA=162
+# KERNEL: READ OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=37 W_EN=1 r_EN=0 ADDRESS=10 w_DATA=110
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=37 W_EN=1 r_EN=0 ADDRESS=10 w_DATA=110
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=37 W_EN=0 r_EN=1 ADDRESS=5 w_DATA=246 R_DATA=138
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=37 W_EN=0 r_EN=1 ADDRESS=5 w_DATA=246,R_DATA=138
+# KERNEL: READ OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: __________________________________________________________________________________________________________
+# KERNEL: ===================GENERATOR SIGNALS==================
+# KERNEL: TIME=39 W_EN=0 r_EN=1 ADDRESS=2 w_DATA=149
+# KERNEL: ====================DRIVER SIGNALS==================
+# KERNEL: TIME=39 W_EN=0 r_EN=1 ADDRESS=2 w_DATA=149
+# KERNEL: ====================MONITOR SIGNALS==================
+# KERNEL: TIME=39 W_EN=1 r_EN=0 ADDRESS=10 w_DATA=110 R_DATA=138
+# KERNEL: ====================SCOREBOARD SIGNALS==================
+# KERNEL: TIME=39 W_EN=1 r_EN=0 ADDRESS=10 w_DATA=110,R_DATA=138
+# KERNEL: WRITE OPERATION SUCCESS
+# KERNEL: __________________________________________________________________________________________________________
