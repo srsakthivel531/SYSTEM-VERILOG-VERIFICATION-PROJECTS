88d415e3907f249a93050adc17f92d9eaa269453
Sakthivel S <srsakthivel531@gmail.com>
2026-06-03 16:39:26 +0530

    Create 08_ENVIRONMENT.sv

---
+++ 05_DUAL PORT RAM/08_ENVIRONMENT.sv
@@ -0,0 +1,33 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+class environment;
+generator gen;
+driver drv;
+monitor mon;
+scoreboard sco;
+mailbox #(transaction)gen_to_drv;
+mailbox #(transaction)mon_to_sco;
+virtual dpram_if vif;
+
+function new(virtual dpram_if vif);
+this.vif=vif;
+gen_to_drv=new();
+mon_to_sco=new();
+gen=new(gen_to_drv,vif);
+drv=new(gen_to_drv,vif);
+mon=new(mon_to_sco,vif);
+sco=new(mon_to_sco);
+endfunction
+
+task run();
+fork
+gen.run();
+drv.run();
+mon.run();
+sco.run();
+join
+endtask
+endclass
