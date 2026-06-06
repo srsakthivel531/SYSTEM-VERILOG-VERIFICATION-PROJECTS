47643b45a26e465f7f8cfc1679e7ae71b9fff724
Sakthivel S <srsakthivel531@gmail.com>
2026-06-06 16:24:16 +0530

    Update 07_SCOREBOARD.sv

--- 04_RAM/07_SCOREBOARD.sv
+++ 04_RAM/07_SCOREBOARD.sv
@@ -8,7 +8,7 @@ this.mon_to_sco=mon_to_sco;
 endfunction 
   
 task run();
-repeat(20) begin
+  repeat(220) begin
 mon_to_sco.get(trans);
  $display("====================SCOREBOARD SIGNALS==================");
 $display("TIME=%0t W_EN=%0d r_EN=%0d ADDRESS=%0d w_DATA=%0d,R_DATA=%0d",$time,trans.w_en,trans.r_en,trans.address,trans.w_data,trans.r_data);
